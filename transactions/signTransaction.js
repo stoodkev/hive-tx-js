a51f9e89ca7fbb9d6eb7d64b97e5ea16a71009da
stoodkev <quentin.correa@gmail.com>
2021-02-22 20:47:00 +0800

    add logs

--- transactions/signTransaction.js
+++ transactions/signTransaction.js
@@ -1,8 +1,8 @@
-const ByteBuffer = require('bytebuffer')
-const config = require('../config')
-const serializer = require('../helpers/serializer')
-const crypto = require('../helpers/crypto')
-const CHAIN_ID = Buffer.from(config.chain_id, 'hex')
+const ByteBuffer = require("bytebuffer");
+const config = require("../config");
+const serializer = require("../helpers/serializer");
+const crypto = require("../helpers/crypto");
+const CHAIN_ID = Buffer.from(config.chain_id, "hex");
 
 /**
  * Sign a transaction by keys (supports multi signature)
@@ -10,39 +10,43 @@ const CHAIN_ID = Buffer.from(config.chain_id, 'hex')
  * @param keys - Array of keys<Buffer>
  */
 const signTransaction = (transaction, keys) => {
-  const CHAIN_ID = Buffer.from(config.chain_id, 'hex')
-  const { digest, txId } = transactionDigest(transaction, CHAIN_ID)
-  const signedTransaction = { ...transaction }
+  const CHAIN_ID = Buffer.from(config.chain_id, "hex");
+  const { digest, txId } = transactionDigest(transaction, CHAIN_ID);
+  const signedTransaction = { ...transaction };
   if (!signedTransaction.signatures) {
-    signedTransaction.signatures = []
+    signedTransaction.signatures = [];
   }
   if (!Array.isArray(keys)) {
-    keys = [keys]
+    keys = [keys];
   }
   for (const key of keys) {
-    const signature = key.sign(digest)
-    signedTransaction.signatures.push(signature.customToString())
+    const signature = key.sign(digest);
+    signedTransaction.signatures.push(signature.customToString());
   }
 
-  return { signedTransaction, txId }
-}
+  return { signedTransaction, txId };
+};
 
 /** Serialize transaction */
 const transactionDigest = (transaction, chainId = CHAIN_ID) => {
   const buffer = new ByteBuffer(
     ByteBuffer.DEFAULT_CAPACITY,
     ByteBuffer.LITTLE_ENDIAN
-  )
+  );
   try {
-    serializer.Transaction(buffer, transaction)
+    console.log(buffer, transaction);
+    serializer.Transaction(buffer, transaction);
   } catch (cause) {
-    throw new Error('Unable to serialize transaction')
+    throw new Error("Unable to serialize transaction");
   }
-  buffer.flip()
-  const transactionData = Buffer.from(buffer.toBuffer())
-  const txId = crypto.sha256(transactionData).toString('hex').slice(0, 40)
-  const digest = crypto.sha256(Buffer.concat([chainId, transactionData]))
-  return { digest, txId }
-}
+  buffer.flip();
+  const transactionData = Buffer.from(buffer.toBuffer());
+  const txId = crypto
+    .sha256(transactionData)
+    .toString("hex")
+    .slice(0, 40);
+  const digest = crypto.sha256(Buffer.concat([chainId, transactionData]));
+  return { digest, txId };
+};
 
-module.exports = signTransaction
+module.exports = signTransaction;
