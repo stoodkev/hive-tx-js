4acb96a5d5f4cbc0321b5047d604b82e63fc9b28
stoodkev <quentin.correa@gmail.com>
2021-02-22 19:37:17 +0800

    add global Buffer

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const signTransaction = require('./transactions/signTransaction')
-const createTransaction = require('./transactions/createTransaction')
-const broadcastTransaction = require('./transactions/broadcastTransaction')
-const broadcastTransactionNoResult = require('./transactions/broadcastTransactionNoResult')
-const PrivateKey = require('./helpers/PrivateKey')
-const call = require('./helpers/call')
-const config = require('./config')
-const updateOperations = require('./helpers/serializer').updateOperations
-
+const signTransaction = require("./transactions/signTransaction");
+const createTransaction = require("./transactions/createTransaction");
+const broadcastTransaction = require("./transactions/broadcastTransaction");
+const broadcastTransactionNoResult = require("./transactions/broadcastTransactionNoResult");
+const PrivateKey = require("./helpers/PrivateKey");
+const call = require("./helpers/call");
+const config = require("./config");
+const updateOperations = require("./helpers/serializer").updateOperations;
+const bf = require("buffer/").Buffer;
+console.log(bf);
+global.Buffer = bf;
 /** Transaction for Steem blockchain */
 class Transaction {
   /** A transaction object could be passed or created later
    * @param {{}} trx Object of transaction - Optional
    */
   constructor(trx = null) {
-    this.created = true
+    this.created = true;
     if (!trx) {
-      this.created = false
+      this.created = false;
     }
-    this.transaction = trx
+    this.transaction = trx;
   }
 
   /** Create the transaction by operations
@@ -25,9 +27,9 @@ class Transaction {
    * @param {Number} expiration Optional - Default 60 seconds
    */
   async create(operations, expiration = 60) {
-    this.transaction = await createTransaction(operations)
-    this.created = true
-    return this.transaction
+    this.transaction = await createTransaction(operations);
+    this.created = true;
+    return this.transaction;
   }
 
   /** Sign the transaction by key or keys[] (supports multi signature)
@@ -35,23 +37,23 @@ class Transaction {
    */
   sign(keys) {
     if (!this.created) {
-      throw new Error('First create a transaction by .create(operations)')
+      throw new Error("First create a transaction by .create(operations)");
     }
-    const { signedTransaction, txId } = signTransaction(this.transaction, keys)
-    this.signedTransaction = signedTransaction
-    this.txId = txId
-    return this.signedTransaction
+    const { signedTransaction, txId } = signTransaction(this.transaction, keys);
+    this.signedTransaction = signedTransaction;
+    this.txId = txId;
+    return this.signedTransaction;
   }
 
   async broadcast() {
     if (!this.created) {
-      throw new Error('First create a transaction by .create(operations)')
+      throw new Error("First create a transaction by .create(operations)");
     }
     if (!this.signedTransaction) {
-      throw new Error('First sign the transaction by .sign(keys)')
+      throw new Error("First sign the transaction by .sign(keys)");
     }
-    const result = await broadcastTransaction(this.signedTransaction)
-    return result
+    const result = await broadcastTransaction(this.signedTransaction);
+    return result;
   }
 
   /** Fast broadcast - No open connection
@@ -59,18 +61,18 @@ class Transaction {
    */
   async broadcastNoResult() {
     if (!this.created) {
-      throw new Error('First create a transaction by .create(operations)')
+      throw new Error("First create a transaction by .create(operations)");
     }
     if (!this.signedTransaction) {
-      throw new Error('First sign the transaction by .sign(keys)')
+      throw new Error("First sign the transaction by .sign(keys)");
     }
-    await broadcastTransactionNoResult(this.signedTransaction)
+    await broadcastTransactionNoResult(this.signedTransaction);
     return {
       id: 1,
-      jsonrpc: '2.0',
-      result: { tx_id: this.txId, status: 'unkown' }
-    } // result
+      jsonrpc: "2.0",
+      result: { tx_id: this.txId, status: "unkown" }
+    }; // result
   }
 }
 
-module.exports = { Transaction, PrivateKey, call, config, updateOperations }
+module.exports = { Transaction, PrivateKey, call, config, updateOperations };
