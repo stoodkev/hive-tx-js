41c299eb8fb0f163b9cbbfcae49f6f9bcb196fe4
stoodkev <quentin.correa@gmail.com>
2021-02-22 21:18:25 +0800

    check  tx creation

--- config.js
+++ config.js
@@ -1,9 +1,8 @@
 const config = {
-  node: 'https://api.hive.blog',
-  // chain_id: '0000000000000000000000000000000000000000000000000000000000000000',
-  chain_id: 'beeab0de00000000000000000000000000000000000000000000000000000000',
-  address_prefix: 'STM',
+  node: "https://api.hive.blog",
+  chain_id: "beeab0de00000000000000000000000000000000000000000000000000000000",
+  address_prefix: "STM",
   rebranded_api: false
-}
+};
 
-module.exports = config
+module.exports = config;
--- transactions/createTransaction.js
+++ transactions/createTransaction.js
@@ -1,35 +1,37 @@
-const globalProps = require('../helpers/globalProps')
-const hfVersion = require('../helpers/hfVersion')
-const config = require('../config')
+const globalProps = require("../helpers/globalProps");
+const hfVersion = require("../helpers/hfVersion");
+const config = require("../config");
 
-const HF23_CHAIN_ID =
-  '0000000000000000000000000000000000000000000000000000000000000000'
 const HF24_CHAIN_ID =
-  'beeab0de00000000000000000000000000000000000000000000000000000000'
+  "beeab0de00000000000000000000000000000000000000000000000000000000";
 
 /** Create transaction by operations */
 const createTransaction = async (operations, exp) => {
-  const expireTime = exp ? 1000 * exp : 1000 * 60
-  const props = await globalProps()
-  const HFV = await hfVersion()
-  if (HFV === '0.23.0') {
-    config.chain_id = HF23_CHAIN_ID
-  } else {
-    config.chain_id = HF24_CHAIN_ID
-  }
-  const refBlockNum = props.head_block_number & 0xffff
-  const refBlockPrefix = Buffer.from(props.head_block_id, 'hex').readUInt32LE(4)
+  const expireTime = exp ? 1000 * exp : 1000 * 60;
+  config.chain_id = HF24_CHAIN_ID;
+  const props = await globalProps();
+  const refBlockNum = props.head_block_number & 0xffff;
+  const refBlockPrefix = Buffer.from(props.head_block_id, "hex").readUInt32LE(
+    4
+  );
   const expiration = new Date(Date.now() + expireTime)
     .toISOString()
-    .slice(0, -5)
-  const extensions = []
+    .slice(0, -5);
+  const extensions = [];
+  console.log({
+    expiration,
+    extensions,
+    operations,
+    ref_block_num: refBlockNum,
+    ref_block_prefix: refBlockPrefix
+  });
   return {
     expiration,
     extensions,
     operations,
     ref_block_num: refBlockNum,
     ref_block_prefix: refBlockPrefix
-  }
-}
+  };
+};
 
-module.exports = createTransaction
+module.exports = createTransaction;
